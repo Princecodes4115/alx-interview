685a48d4460bc5380be5ddd0cbc471b57f3441bc
Ezekiel Moses <codewithprince.com@gmail.com>
2023-08-04 00:22:32 +0100

    error handling

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,52 +1,45 @@
+#!/usr/bin/python3
+""" N queens """
 import sys
 
-def is_safe(board, row, col):
-    # Check if there is a queen in the same column
-    for i in range(row):
-        if board[i] == col:
-            return False
 
-        # Check if there is a queen in the same diagonal
-        if abs(board[i] - col) == abs(i - row):
-            return False
+if len(sys.argv) > 2 or len(sys.argv) < 2:
+    print("Usage: nqueens N")
+    exit(1)
 
-    return True
+if not sys.argv[1].isdigit():
+    print("N must be a number")
+    exit(1)
 
-def print_board(board):
-    n = len(board)
-    for row in range(n):
-        print([row, board[row]])
+if int(sys.argv[1]) < 4:
+    print("N must be at least 4")
+    exit(1)
 
-def solve_nqueens(n, row=0, board=None):
-    if board is None:
-        board = [-1] * n
+n = int(sys.argv[1])
 
-    if row == n:
-        print_board(board)
-        return
 
-    for col in range(n):
-        if is_safe(board, row, col):
-            board[row] = col
-            solve_nqueens(n, row + 1, board)
-            board[row] = -1
+def queens(n, i=0, a=[], b=[], c=[]):
+    """ find possible positions """
+    if i < n:
+        for j in range(n):
+            if j not in a and i + j not in b and i - j not in c:
+                yield from queens(n, i + 1, a + [j], b + [i + j], c + [i - j])
+    else:
+        yield a
 
-def main():
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
 
-    try:
-        N = int(sys.argv[1])
-        if N < 4:
-            print("N must be at least 4")
-            sys.exit(1)
+def solve(n):
+    """ solve """
+    k = []
+    i = 0
+    for solution in queens(n, 0):
+        for s in solution:
+            k.append([i, s])
+            i += 1
+        print(k)
+        k = []
+        i = 0
 
-        solve_nqueens(N)
-    except ValueError:
-        print("N must be a number")
-        sys.exit(1)
 
-if __name__ == "__main__":
-    main()
+solve(n)
 
